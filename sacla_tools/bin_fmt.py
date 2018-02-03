7de3e3d1a38f7185dd52d3e1c8d3256b60e6cd95
Daehyun You <daehyun.park.you@gmail.com>
2018-02-03 20:57:11 +0900

    Update old codes

--- sacla_tools/bin_fmt.py
+++ sacla_tools/bin_fmt.py
@@ -29,13 +29,15 @@ def hit_reader(filename) -> Generator[dict, None, None]:
             }
 
 
-def bin_reader(filename) -> Generator[dict, None, None]:
+def bin_reader(filename, keys=None) -> Generator[dict, None, None]:
     """
     Example:
         for d in bin_reader('aq137.bin'):
             print(d)
             break
     """
+    if keys is None:
+        keys = tuple('meta{}'.format(i) for i in range(8))
     deep1 = Struct('=IBBBBddddI')
     unpack1 = deep1.unpack
     size1 = deep1.size
@@ -48,18 +50,10 @@ def bin_reader(filename) -> Generator[dict, None, None]:
         seek = f.seek
         while read(1):
             seek(-1, 1)
-            (tag, fel_status, fel_shutter, x1_is_on, x2_is_on,
-             fel_gas_monitor, x1_intensity, x2_intensity, delay, nhits) = unpack1(read(size1))
+            tag, *meta, nhits = unpack1(read(size1))
             yield {
                 'tag': tag,
-                'fel_status': fel_status,
-                'fel_shutter': fel_shutter,
-                'x1_is_on': x1_is_on,
-                'x2_is_on': x2_is_on,
-                'fel_gas_monitor': fel_gas_monitor,
-                'x1_intensity': x1_intensity,
-                'x2_intensity': x2_intensity,
-                'delay': delay,
+                **dict(zip(keys, meta)),
                 'nhits': nhits,
                 'hits': [dict(zip(('t', 'x', 'y'), unpack2(read(size2)))) for _ in range(nhits)]
             }
