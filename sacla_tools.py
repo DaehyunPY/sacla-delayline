8d01693b8c797a705e17cd41c4fc70e7fd808227
Daehyun You <daehyun@dc.tohoku.ac.jp>
2018-02-16 01:23:20 +0900

    Raise warning message when import module 'sacla_tools'

---
+++ sacla_tools.py
@@ -0,0 +1,6 @@
+from warnings import warn
+
+from saclatools import *
+
+warn("Module 'sacla_tools' is renamed to 'saclatools'!", DeprecationWarning)
+
--- saclatools/__init__.py
+++ saclatools/__init__.py
@@ -1,3 +1,5 @@
 from .bin_fmt import hit_reader, bin_reader
 from .lma_fmt import LmaReader
 from .sacla_db import tags_at, scalars_at, ArrReader
+
+__all__ = ('hit_reader', 'bin_reader', 'LmaReader', 'tags_at', 'scalars_at', 'ArrReader')
