5138dcd1089a5b7543cb53efb0574f07754b8ecc
Daehyun You <daehyun.park.you@gmail.com>
2018-07-20 00:29:29 +0900

    Backup

--- pcov.py
+++ pcov.py
@@ -1,58 +1,82 @@
+# -*- coding: utf-8 -*-
+from random import shuffle
 from pprint import pprint
-from glob import iglob
-from h5py import File
+from functools import lru_cache
 
-from dask.delayed import delayed
-from dask.array import concatenate, cov, from_array, from_delayed
-from dask.diagnostics import ProgressBar
+from dask.array import cov
+from numpy import histogram, append, linspace, stack
 from matplotlib import pyplot as plt
+from cytoolz import concat, take
 
-filename = ("/work/uedalab/"
-            "/2017B8050/hdf_files/Aq{:03d}__*.h5".format)
-
-filenames = sorted(iglob(filename(67)))
-pprint(filenames)
-
-
-#
-# # %%
-# shapes = [File(fn, 'r')['/channel6'].shape for fn in filenames]
-# arrs = concatenate([from_array(File(fn, 'r')['/channel6'], chunks=sh) for fn, sh in zip(filenames, shapes)])
-# arr0 = concatenate([from_array(File(fn, 'r')['/fel_intensity'], chunks=sh) for fn, (sh, _) in zip(filenames, shapes)])
-# arr = concatenate([arr0[:, None], arrs[:, 2500:6000]], axis=1)
-#
-# with ProgressBar():
-#     img = cov(arr.T).compute()
-#     pprint(img)
-#
-# img_cov = img[1:, 1:]
-# img_icov = img[1:, 0][:, None] @ img[0, 1:][None, :] / img[0, 0]
-# img_pcov = img_cov - img_icov
+from saclatools import bin_reader as __bin_reader
 
 
 # %%
-@delayed
-def target(filename):
-    with File(filename, 'r') as f:
-        return concatenate([f['/fel_intensity'][...][:, None], f['/channel6'][:, 2500:6000]], axis=1).astype('float32')
+@lru_cache()
+def bin_reader(filename):
+    keys = ("fel_status", "fel_shutter", "uv_shutter", "meta3",
+            "fel_intensity", "delay", "meta6", "meta7")
+    print("Loading a file {}...".format(filename))
+    loaded = tuple(__bin_reader(filename, keys=keys))
+    pprint(loaded[0])
+    return loaded
 
 
-shapes = [File(fn, 'r')['/tags'].shape[0] for fn in filenames]
-arr = concatenate([from_delayed(target(fn), shape=[sh, 3501], dtype='float32') for fn, sh in zip(filenames, shapes)])
+# %%
+filename = "/Users/daehyun/Documents/sacla-delayline/Aq{:03d}.bin".format
+run = 67
+events = bin_reader(filename(run))
+hits = list(concat(d['hits'] for d in events))
+shuffle(hits)
+hit_stream = iter(hits)
+shuffled_events = (
+    {'hits': take(d['nhits'], hit_stream),
+     'fel_intensity': d['fel_intensity']}
+    for d in events
+)
 
-with ProgressBar():
+
+bins = linspace(2000, 8000, 601)
+
+
+def hit_map(event):
+    t = (h['t'] for h in event['hits'])
+    hist, _ = histogram(tuple(t), bins)
+    return append(event['fel_intensity'], hist)
+
+
+def par_cov(arr):
     img = cov(arr.T).compute()
-    pprint(img)
+    img_cov = img[1:, 1:]
+    img_icov = img[1:, 0][:, None] @ img[0, 1:][None, :] / img[0, 0]
+    return img_cov - img_icov
+    # return img_cov
 
-img_cov = img[1:, 1:]
-img_icov = img[1:, 0][:, None] @ img[0, 1:][None, :] / img[0, 0]
-img_pcov = img_cov - img_icov
 
-#
-# # %%
-# plt.figure()
-# plt.pcolormesh(img_pcov, cmap='RdBu')
-# plt.colorbar()
-# plt.clim(-100, 10)
-# plt.savefig('tmp.png')
-# plt.show()
+pcov = par_cov(stack(tuple(hit_map(d) for d in events)))
+pcov_shuffled = par_cov(stack(tuple(hit_map(d) for d in shuffled_events)))
+
+
+# %%
+plt.figure(figsize=(5, 15))
+plt.subplot(311)
+plt.pcolormesh(bins, bins, pcov, cmap='RdBu')
+plt.title("pcov map (run {})".format(run))
+plt.xlim(2500, 6000)
+plt.ylim(2500, 6000)
+plt.clim(-0.0001, 0.0001)
+
+plt.subplot(312)
+plt.pcolormesh(bins, bins, pcov_shuffled, cmap='RdBu')
+plt.title("pcov map from shuffled events (run {})".format(run))
+plt.xlim(2500, 6000)
+plt.ylim(2500, 6000)
+plt.clim(-0.0001, 0.0001)
+
+plt.subplot(313)
+plt.pcolormesh(bins, bins, pcov-pcov_shuffled, cmap='RdBu')
+plt.title("differential (run {})".format(run))
+plt.xlim(2500, 6000)
+plt.ylim(2500, 6000)
+plt.clim(-0.0001, 0.0001)
+plt.show()
